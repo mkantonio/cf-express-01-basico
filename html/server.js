04a2ad2c3802c5a6047ad8b23864541539ca4f7a
Marco <mkchoque@gmail.com>
2022-10-20 11:58:48 -0400

    usando paquete EJS para renderizar HTML

--- html/server.js
+++ html/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use('/assets',express.static('assets'));
-// app.use('/public',express.static('assets'));
+app.set('view engine', 'ejs');
+// app.use('/assets',express.static('assets'));
+app.use('/public',express.static('assets'));
 
 app.get('/', function(req, res){
     res.sendFile('index.html', {
         root: __dirname
     });
+});
+
+app.get('/ejs', function(req, res){
+    res.render('index');
 });
 
 app.listen(3000);
