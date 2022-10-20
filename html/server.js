07079a8e8bd8e4582406248c590e5cbd672c9405
Marco <mkchoque@gmail.com>
2022-10-20 00:47:53 -0400

    envio de html

---
+++ html/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {
+        root: __dirname
+    });
+});
+
+app.listen(3000);
