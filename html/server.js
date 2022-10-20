044acc254824d4f513ba602accaaefd1bacbda97
Marco <mkchoque@gmail.com>
2022-10-20 12:14:38 -0400

    configurando el cache y caducidad de archivos

--- html/server.js
+++ html/server.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 // app.use('/assets',express.static('assets'));
-app.use('/public',express.static('assets'));
+app.use('/public',express.static('assets'), {
+    etag: false,
+    maxAge: '5h'
+}); //https://github.com/vercel/ms //el archivo tendrá vigencia por 5 horas.
 
 app.get('/', function(req, res){
     res.sendFile('index.html', {
