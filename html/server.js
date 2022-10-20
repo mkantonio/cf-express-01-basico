ce58eb66f0c722644f13489a1227e73b1c2669ac
Marco <mkchoque@gmail.com>
2022-10-20 00:58:19 -0400

    servidor de archivos estaticos

--- html/server.js
+++ html/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use('/assets',express.static('assets'));
+// app.use('/public',express.static('assets'));
+
 app.get('/', function(req, res){
     res.sendFile('index.html', {
         root: __dirname
